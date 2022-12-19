f3675fe7309c2cddf96dd1b2931c806b22a1c93f
yildiztugba <okra.yildiz@gmail.com>
2022-12-19 21:50:26 +0100

    Composable! challange 2

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
 // ReactDOM.render(<h1>hello</h1>,document.getElementById("root"));
 
+
+//CHALLENGE #1
 /* Surprise! You probably thought you could just forget
 the line of code you just learned! Nope, not on my watch!
 
 Try to write that 1-liner of React code again! This time,
 see if you can figure out how to render an <ul> with 2+ <li>s inside*/
 
-ReactDOM.render(<ul><li>one</li><li>two</li></ul>, document.getElementById("root"));
+//ReactDOM.render(<ul><li>one</li><li>two</li></ul>, document.getElementById("root"));
+
+
+
+
+
+
+//CHALLENGE #2
+// Challenge: Create your own custom React component!
+// Call it "MainContent", and have it return a simple
+// h1 element that says "I'm learning React!"
+
+// Afterward, render it below the Navbar (which
+// you can do inside the ReactDOM.render call below)
+
+
+function MainContent () {
+    return <h1>I'm learning React!</h1>
+} 
+
+ReactDOM.render(
+    <div>
+        <Navbar />
+        <MainContent />
+        
+    </div>,
+    document.getElementById("root")
+)
