2d1de66ad50e5131414ed3463cb239b1e7b711d9
yildiztugba <okra.yildiz@gmail.com>
2022-12-19 21:32:29 +0100

    first react practice

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-ReactDOM.render(<h1>hello</h1>,document.getElementById("root"));
+// ReactDOM.render(<h1>hello</h1>,document.getElementById("root"));
+
+/* Surprise! You probably thought you could just forget
+the line of code you just learned! Nope, not on my watch!
+
+Try to write that 1-liner of React code again! This time,
+see if you can figure out how to render an <ul> with 2+ <li>s inside*/
+
+ReactDOM.render(<ul><li>one</li><li>two</li></ul>, document.getElementById("root"));
