8dcebf22728657fd7d13bd8f22e6b95b1b2d5f34
yildiztugba <okra.yildiz@gmail.com>
2022-12-19 22:08:54 +0100

    JSX Challange 3

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ see if you can figure out how to render an <ul> with 2+ <li>s inside*/
 // Afterward, render it below the Navbar (which
 // you can do inside the ReactDOM.render call below)
 
-
+/*
 function MainContent () {
     return <h1>I'm learning React!</h1>
 } 
@@ -36,3 +36,27 @@ ReactDOM.render(
     </div>,
     document.getElementById("root")
 )
+*/
+
+    // CHALLANGE #3"
+    /*Create a navbar in JSX:
+    - Use the semantic `nav` element as the parent wrapper
+    - Have an h1 element with the brand name of your "website"
+    - Insert an unordered list for the other nav elements
+        - Inside the `ul`, have three `li`s for "Pricing",
+        "About", and "Contact"
+    - Don't worry about styling yet - it'll just be plain-looking HTML for now
+*/
+
+const navbar = (
+    <nav>
+        <h1>Bob's Bistro</h1>
+        <ul>
+            <li>Menu</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+)
+
+ReactDOM.render(navbar, document.getElementById("root"))
