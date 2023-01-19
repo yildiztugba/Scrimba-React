3fd69ab092fa4c279ac021d9e34bf318e6de4c29
yildiztugba <okra.yildiz@gmail.com>
2023-01-19 10:42:02 +0100

    challange

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ ReactDOM.render(
     - Don't worry about styling yet - it'll just be plain-looking HTML for now
 */
 
-const navbar = (
+/*const navbar = (
     <nav>
         <h1>Bob's Bistro</h1>
         <ul>
@@ -60,3 +60,36 @@ const navbar = (
 )
 
 ReactDOM.render(navbar, document.getElementById("root"))
+*/
+
+/*
+Challenge: Starting from scratch, build and render the 
+HTML for our section project. Check the Google slide for 
+what you're trying to build.
+
+We'll be adding styling to it later.
+
+Hints:
+* The React logo is a file in the project tree, so you can
+  access it by using `src="./react-logo.png" in your image
+  element
+* You can also set the `width` attribute of the image element
+  just like in HTML. In the slide, I have it set to 40px
+ */
+
+
+const page = (
+    <>
+    <img width="40px" src="./react-logo.png" />
+    <h1>Fun facts about React</h1>
+    <ul>
+        <li>Was first released in 2023</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on Github</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+    </ul>
+    </>
+)
+
+ReactDOM.render(page, document.getElementById("root"))
