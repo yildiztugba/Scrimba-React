22d69a9ec933409ca88f6bf76d089d87aceff926
yildiztugba <okra.yildiz@gmail.com>
2023-01-20 13:49:20 +0100

    organize component challange

---
+++ custom components/organize component/index.js
@@ -0,0 +1,21 @@
+import Header from "./Header"
+
+/**
+Challenge: move the Footer and MainContent components
+into their own files.
+*/
+
+import Footer from "./Footer"
+import MainContent from "./MainContent"
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <MainContent />
+            <Footer />
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById("root"))
