b70cb3005f39fbbb6a4b22d5e0fef16c6e218770
yildiztugba <okra.yildiz@gmail.com>
2023-02-01 10:02:40 +0100

    image,p

--- my-app/src/components/Card.js
+++ my-app/src/components/Card.js
@@ -1,5 +1,13 @@
 import React from "react"
 
 export default function Card() {
-    return ()
+    return (
+        <div className="card">
+            <img src="../images/katie-zaferes.png" className="card--image" />
+            <div className="card--stats">
+            </div>
+            <p>Life Lessons with Katie Zaferes</p>
+            <p><span className="bold">From $136</span> / person</p>
+        </div>
+    )
 }
