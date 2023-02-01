abda3bd9e750e1406a437a5d95f29677ddee7ac7
yildiztugba <okra.yildiz@gmail.com>
2023-02-01 10:03:27 +0100

    card stat

--- my-app/src/components/Card.js
+++ my-app/src/components/Card.js
@@ -5,6 +5,10 @@ export default function Card() {
         <div className="card">
             <img src="../images/katie-zaferes.png" className="card--image" />
             <div className="card--stats">
+                <img src="../images/star.png" className="card--star" />
+                <span>5.0</span>
+                <span className="gray">(6) • </span>
+                <span className="gray">USA</span>
             </div>
             <p>Life Lessons with Katie Zaferes</p>
             <p><span className="bold">From $136</span> / person</p>
